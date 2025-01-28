b7127588945c01ed7c5c633bae947f91334c50c5
Issa Oulkhir <aissaoulkhir22@gmail.com>
2025-01-28 01:10:23 +0100

    no leaks

--- child1.c
+++ child1.c
@@ -6,7 +6,7 @@
 /*   By: ioulkhir <ioulkhir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 10:13:31 by ioulkhir          #+#    #+#             */
-/*   Updated: 2025/01/28 00:49:21 by ioulkhir         ###   ########.fr       */
+/*   Updated: 2025/01/28 01:09:55 by ioulkhir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,17 @@ void	child1(int pipe_fds[2], t_parsed_data data)
 {
 	int	fd_in;
 
+	free_split(data.cmd2, 0);
 	close(pipe_fds[0]);
 	dup2_and_close(data, STDOUT_FILENO, pipe_fds[1]);
 	fd_in = open(data.infile, 0);
 	if (fd_in == -1)
 	{
+		free_split(data.cmd1, 0);
 		perror(data.infile);
 		exit(EXIT_FAILURE);
 	}
 	dup2_and_close(data, STDIN_FILENO, fd_in);
-	free_split(data.cmd2, 0);
 	execve(data.cmd1[0], data.cmd1, data.env);
 	free_split(data.cmd1, 0);
 	display_err("First command failure");
--- child2.c
+++ child2.c
@@ -6,7 +6,7 @@
 /*   By: ioulkhir <ioulkhir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 10:13:31 by ioulkhir          #+#    #+#             */
-/*   Updated: 2025/01/28 00:27:03 by ioulkhir         ###   ########.fr       */
+/*   Updated: 2025/01/28 01:10:03 by ioulkhir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,17 @@ void	child2(int pipe_fds[2], t_parsed_data data)
 {
 	int	fd_out;
 
+	free_split(data.cmd1, 0);
 	close(pipe_fds[1]);
 	dup2_and_close(data, STDIN_FILENO, pipe_fds[0]);
 	fd_out = open(data.outfile, O_WRONLY | O_CREAT | O_TRUNC, 0777);
 	if (fd_out == -1)
 	{
+		free_split(data.cmd2, 0);
 		perror(data.outfile);
 		exit(EXIT_FAILURE);
 	}
 	dup2_and_close(data, STDOUT_FILENO, fd_out);
-	free_split(data.cmd1, 0);
 	execve(data.cmd2[0], data.cmd2, data.env);
 	free_split(data.cmd2, 0);
 	display_err("Second command failure");
